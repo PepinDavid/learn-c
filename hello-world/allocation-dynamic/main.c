0630b89acd09445ccb7ff0e77d370786aee61ea6
David <david.pepin@genicorp.fr>
2017-10-20 10:24:48 +0200

    add testing ptr

--- hello-world/allocation-dynamic/main.c
+++ hello-world/allocation-dynamic/main.c
@@ -25,6 +25,9 @@ struct Game{
     int difficulty;
 };
 
+void triple(int *c){
+    *c *= 3;
+}
 
 int main(int argc, char **argv)
 {
@@ -34,10 +37,11 @@ int main(int argc, char **argv)
     int* ptrInt = NULL;
     char* ptrChar = NULL;
     Player p = {"Pepin", NULL, 10, 0};
-    Game g = {&p};
+    Game g = {NULL};
+    g.p = &p;
     
     
-	printf("sizeof \n");
+	/*printf("sizeof \n");
     printf("char : %ld octets\n", sizeof(char));
     printf("int : %ld octets\n", sizeof(int));
     printf("long : %ld octets\n", sizeof(long));
@@ -49,7 +53,7 @@ int main(int argc, char **argv)
     printf("ptr int : %ld octets\n", sizeof(ptrInt));
     printf("ptr char : %ld octets\n", sizeof(ptrChar));
     
-    printf("Coordonnees : %ld octets\n", sizeof(Coords));
+    printf("Coordonnees : %ld octets\n", sizeof(Coords));*/
     printf("Player : %ld octets\n", sizeof(Player));
     printf("Game : %ld octets\n", sizeof(Game));
     printf("Player edited : %ld octets\n", sizeof(p));
@@ -58,12 +62,13 @@ int main(int argc, char **argv)
     printf("Game edited : %ld octets\n", sizeof(g));
     
     printf("address g : %p\n", &g);
-    printf("address g : %p\n", &(g.p));
+    printf("address g : %p\n", &g.p);
+    printf("address g : %p\n\n", &g.nbMyst);
     printf("address p : %p\n", &p);
-    printf("address p in g : %p\n", (void *)(g.p));
-    printf("address p.surname : %p\n", (void *)(p.surname));
-    //???? printf("address g.p : %p\n", *(g.p));
-    printf("%d\n",(p.surname == NULL));
+    printf("address p in g = g.p : %p\n", g.p);
+    printf("address p in g = (void *)(g.p) : %p\n\n", (void *)(g.p));
+    printf("address surname in p before malloc: %p\n", (void *)(p.surname));
+    
     
     printf("%s\n", p.name);
     printf("Votre prénom : ");
@@ -74,20 +79,40 @@ int main(int argc, char **argv)
             cpt++;
     }
     
-    printf("%d\n", cpt);    
+    printf("cpt : %d\n", cpt);    
     printf("prenom sizeof : %ld\n", sizeof(char)*cpt+1);
     (p.surname) = malloc(sizeof(char)*cpt+1);
     printf("address p.surname : %p\n", (void *)(p.surname));
-    p.surname = arrChar;
+    for(int i = 0; i < strlen(arrChar); i++){
+        if(arrChar[i] != '\0')
+            p.surname[i] = arrChar[i];
+    }
     printf("address p.surname : %p\n", (void *)(p.surname));
     printf("%s\n", p.surname);
     printf("Player surname after : %ld octets\n", sizeof(p.surname));
+    arrChar[0] = "B";
+    arrChar[1] = "l";
+    arrChar[2] = "A";
+    arrChar[3] = "B";
+    arrChar[4] = "L";
+    arrChar[5] = "A";
     
+    printf("%s\n",p.surname);
+    printf("%s\n",arrChar);
     //same things
-    printf("address p.surname : %p\n", *p.surname);
-    printf("address p.surname : %p\n", &p.surname);
+    //printf("address p.surname : %p\n", *(p.surname));
+    //printf("address p.surname : %p\n", &(p.surname));
     
-    printf("address p.surname : %p\n", (void *)(p.surname));
+//    printf("address p.surname : %p\n", (void *)(p.surname));
+
+    int a = 10;
+    int *b = &a;
+    printf("%p\n", &a);
+    printf("%p\n", b);
     
+    triple(b);
+    
+    printf("%d\n",*b);
+    printf("%d\n",a);
 	return 0;
 }
