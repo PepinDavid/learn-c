5db611d05fa7be174179627103cad5a026273d32
David <david.pepin@genicorp.fr>
2017-10-21 16:15:59 +0200

    probleme regler sur l'alloc dyn de word

--- hello-world/hanged-man/hanged-man.mk
+++ hello-world/hanged-man/hanged-man.mk
@@ -5,22 +5,22 @@
 ## Debug
 ProjectName            :=hanged-man
 ConfigurationName      :=Debug
-WorkspacePath          := "/home/david/perso/dev/learn-c/hello-world"
-ProjectPath            := "/home/david/perso/dev/learn-c/hello-world/hanged-man"
+WorkspacePath          :=/home/david/dev/learn-c/hello-world
+ProjectPath            :=/home/david/dev/learn-c/hello-world/hanged-man
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
-User                   :=David
-Date                   :=20/10/17
-CodeLitePath           :="/home/david/.codelite"
-LinkerName             :=g++
-SharedObjectLinkerName :=g++ -shared -fPIC
+User                   :=root
+Date                   :=21/10/17
+CodeLitePath           :=/root/.codelite
+LinkerName             :=/usr/bin/g++
+SharedObjectLinkerName :=/usr/bin/g++ -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
-PreprocessSuffix       :=.o.i
-DebugSwitch            :=-gstab
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
 IncludeSwitch          :=-I
 LibrarySwitch          :=-l
 OutputSwitch           :=-o 
@@ -31,7 +31,7 @@ OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
 Preprocessors          :=
 ObjectSwitch           :=-o 
 ArchiveOutputSwitch    := 
-PreprocessOnlySwitch   :=-E 
+PreprocessOnlySwitch   :=-E
 ObjectsFileList        :="hanged-man.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=mkdir -p
@@ -47,13 +47,13 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := ar rcus
-CXX      := g++
-CC       := gcc
+AR       := /usr/bin/ar rcu
+CXX      := /usr/bin/g++
+CC       := /usr/bin/gcc
 CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := as
+AS       := /usr/bin/as
 
 
 ##
@@ -92,12 +92,12 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/main.c$(ObjectSuffix): main.c $(IntermediateDirectory)/main.c$(DependSuffix)
-	$(CC) $(SourceSwitch) "/home/david/perso/dev/learn-c/hello-world/hanged-man/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
+	$(CC) $(SourceSwitch) "/home/david/dev/learn-c/hello-world/hanged-man/main.c" $(CFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.c$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.c$(DependSuffix): main.c
-	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.c$(ObjectSuffix) -MF$(IntermediateDirectory)/main.c$(DependSuffix) -MM "main.c"
+	@$(CC) $(CFLAGS) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.c$(ObjectSuffix) -MF$(IntermediateDirectory)/main.c$(DependSuffix) -MM main.c
 
 $(IntermediateDirectory)/main.c$(PreprocessSuffix): main.c
-	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.c$(PreprocessSuffix) "main.c"
+	$(CC) $(CFLAGS) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.c$(PreprocessSuffix) main.c
 
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
--- hello-world/hanged-man/main.c
+++ hello-world/hanged-man/main.c
@@ -3,42 +3,65 @@
 #include <string.h>
 #include <time.h>
 
-const char* PATH = "/home/david/perso/dev/learn-c/hello-world/hanged-man/";
+const char* PATH = "/home/david/dev/learn-c/hello-world/hanged-man/";
 const char* FILE_NAME = "dico.txt";
 
+int nomberRand(int nombreMax){
+    srand(time(NULL));
+    return (rand() % nombreMax);
+}
+
 void openFile(char *pathAndFile, int *cpt, char *w){
-	srand(time(NULL)); 
 	FILE *dico = NULL;
 	int charRead = 0;
-	int close = 0;
+    int sizeWord = 1;
+    int posCursor = 0;
+    int ch = 0;
+	//int close = 0;
 	int choosenWord = 0;
 	
+    //on ouvre le fichier
 	dico = fopen(pathAndFile, "r");
 	
 	if(dico != NULL){
-		
+        //on compte le nombre de mot
 		do{
 			charRead = fgetc(dico);
 			if(charRead == '\n')
 				(*cpt)++;
 		}while(charRead != EOF);
 		
+        
 		printf("dico contains : %d words \n", (*cpt)+1);
 		
-		choosenWord = (rand() % *cpt);
+        //choix d'un nombre au hazar
+		choosenWord = nomberRand(*cpt);
 		
-		printf("myst num : %d\n", choosenWord);
-		
+		printf("myst num : %d\n", choosenWord);		
+        //on recommence du debut du fichier
 		rewind(dico);
-		
+        //tant que qu'on est pas a la ligne du mot choisi
 		while(choosenWord > 0){
 			charRead = fgetc(dico);
-			if(charRead == "\n")			
+			if(charRead == '\n')			
 				choosenWord--;
 		}
-		
-		printf("%d", choosenWord);
-		fgets(w, 100, dico);
+        
+        //faire un do while pour attraper le nombre de lettre du mot choisi
+        do{
+            charRead = fgetc(dico);
+            printf("%c",charRead);
+			if(charRead != '\n')
+                sizeWord++;
+        }while(charRead != '\n');
+        
+        //on se remet au debut du mot
+        fseek(dico, -sizeWord, SEEK_CUR);
+        //alloc dyn de la taille du mot +1
+        w = malloc(sizeof(char)*sizeWord+1);
+		fgets(w, sizeWord, dico);
+        //ajout du caractere de fin de mot
+        w[strlen(w)] = '\0';
 
 		fclose(dico);
 	}else{
@@ -49,7 +72,7 @@ void openFile(char *pathAndFile, int *cpt, char *w){
 
 int main(int argc, char **argv)
 {
-	char *pathAndFile = NULL, secretWord[100];
+	char *pathAndFile = NULL, *secretWord = NULL;
 	int cpt = 0;
 	
 	pathAndFile = malloc(strlen(PATH)+sizeof(FILE_NAME)); // make space for new string
@@ -63,9 +86,9 @@ int main(int argc, char **argv)
 	
 	printf("path file : %s\n", pathAndFile);
 	
-	openFile(pathAndFile, &cpt, &secretWord);
+	openFile(pathAndFile, &cpt, secretWord);
 	
-	printf("%s\n", secretWord);
+	//printf("%s\n", secretWord);
 	free(pathAndFile);
 	pathAndFile = NULL;
 	
